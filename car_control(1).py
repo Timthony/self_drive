8097d251c50a855ca598fd08ece438755da9baa1
Timthony <tianhangz1023@gmail.com>
2018-09-29 16:40:36 +0800

    1

--- car_control(1).py
+++ car_control(1).py
@@ -23,15 +23,15 @@ GPIO.setup(frontMotorinput2,GPIO.OUT)            # 此端口为输出模式
 GPIO.setup(backMotorEn,GPIO.OUT)
 GPIO.setup(frontMotorEn,GPIO.OUT)
 # 将控制小车运动封装为函数
-backMotorPwm = GPIO.PWM(backMotorEn,100)
-backMotorPwm.start(0)
+backMotorPwm = GPIO.PWM(backMotorEn,100)         # 配置PWM
+backMotorPwm.start(0)                            # 开始输出PWM
 # 当使能端口输入低电压时，电机驱动板将不对电机输出电流，电机将不工作。
 # 当使能端口输入高电压时，让前轮转向电机正常工作。
 # 向前走
 def carMoveforward():
 	GPIO.output(backMotorinput1,GPIO.HIGH)
 	GPIO.output(backMotorinput2,GPIO.LOW)
-	backMotorPwm.ChangeDutyCycle(speed1)
+	backMotorPwm.ChangeDutyCycle(speed1)         # 改变PWM占空比，参数为占空比
 # 向后退
 def carMovebackward():
 	GPIO.output(backMotorinput1,GPIO.LOW)
@@ -69,10 +69,10 @@ def carbackRight():
 	GPIO.output(backMotorinput1,GPIO.LOW)
 	GPIO.output(backMotorinput2,GPIO.HIGH)
 	backMotorPwm.ChangeDutyCycle(speed2)
-# 初始化端口
+# 清除记忆
 def cleanGPIO():
 	GPIO.cleanup()
-	backMotorPwm.stop()
+	backMotorPwm.stop()                          # 停止输出PWM
 # 前轮回正函数
 def carTurnStraight():
 	GPIO.output(frontMotorEn,GPIO.LOW)    # 当使能端口输入低电压时，电机驱动板将不对电机输出电流，电机将不工作。
